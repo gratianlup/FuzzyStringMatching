b6b736e1af1c4787f6d8680962aacd41d6a1f24b
Gratian Lup <gratianlup@outlook.com>
2013-08-14 14:14:19 +0300

    Update LevenshteinDistance.java

--- src/LevenshteinDistance.java
+++ src/LevenshteinDistance.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 // Copyright (c) 2013 Gratian Lup. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
@@ -32,6 +29,9 @@ import java.util.List;
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevenshteinDistance {
     public static int computeDistance(String word, String pattern) {
         // Implements the classic dynamic-programming algorithm for
